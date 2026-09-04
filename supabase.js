be3f1e117429225ce0f55acf7aa2a0e0cb8a3692
dataapk <onlinedatabase.expert@gmail.com>
2026-09-04 21:37:44 +0600

    Fix supabaseClient assignment to window object

--- supabase.js
+++ supabase.js
@@ -6,7 +6,7 @@ const SUPABASE_URL = "https://dijuduhvleunvwmnncue.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_dw-uENrowls3CoIxyJamJg_vtpR7wR3";
 
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
